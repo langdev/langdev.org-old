4e98c210ea87d9ae743b001218cd021f5f9c6f6f
Hong Minhee <minhee@dahlia.kr>
2011-05-13 11:08:31 +0900

    Initial commit.

---
+++ langdev/__init__.py
@@ -0,0 +1,28 @@
+""":mod:`langdev` --- LangDev_
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. note::
+
+   This package is namespace package. You can extend the :mod:`idgraph`
+   package in separated filesystem path. In order to do that, creates a
+   folder named :file:`langdev`. Then, save the :file:`langdev/__init__.py`
+   file of the following content::
+
+       ''':mod:`langdev` --- LangDev
+       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+       '''
+       from pkgutil import extend_path
+       __path__ = extend_path(__path__, __name__)
+
+   Note that you must not make any variables/functions/class in the package,
+   and creates modules like :mod:`langdev.foobar` under the package instead.
+
+   .. seealso:: :pep:`382` --- Namespace Packages
+
+.. _LangDev: http://langdev.org/
+
+"""
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import distutils.util
+
+
+def find_packages(where='.'):
+    """Borrowed from `Distribute <http://packages.python.org/distribute/>`_."""
+    out = []
+    stack=[(distutils.util.convert_path(where), '')]
+    while stack:
+        where,prefix = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where,name)
+            if ('.' not in name and os.path.isdir(fn) and
+                os.path.isfile(os.path.join(fn, '__init__.py'))):
+                out.append(prefix+name); stack.append((fn, prefix + name + '.'))
+    for pat in 'ez_setup', 'distribute_setup':
+        from fnmatch import fnmatchcase
+        out = [item for item in out if not fnmatchcase(item, pat)]
+    return out
+
+
+setup(name='LangDev',
+      version='0.1',
+      url='http://langdev.org/',
+      author='Hong Minhee',
+      author_email='minhee' '@' 'dahlia.kr',
+      description='Programming language implementors and designers community.',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      license='AGPLv3')
+
